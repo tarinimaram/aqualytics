075939a5e25d462305a6852f745b55148ca01704
smartindelcampo-blip <smartindelcampo@ucdavis.edu>
2025-11-12 20:18:37 -0800

    play class made

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 class Player:
     def __init__(self, name, number) -> None:
+        # Could include biometric data; Height, Weight, Swimspeed, etc.
         self.name = name
         self.number = number
         self.game_stats = [] # list of PlayerGameStat
 
+
     def fillGameStats():
         pass
-    
+   
     def getAvgStats():
         pass
+
 
 class PlayerGameStat:
     def __init__(self, name, number, shots, goals, blocks, rebounds, assists, steals, turnovers, hustles, exclusions, tipped_passes) -> None:
@@ -27,4 +30,12 @@ class PlayerGameStat:
         self.tipped_passes = tipped_passes
 
 
+class Plays:
+    def __init__(self, play_name, Names, Numbers, timestamp, success):
+        self.play_name = play_name
+        self.names = Names # list of names of players apart of play
+        self.numbers = Numbers # list of numbers apart of play
+        self.timestamp = timestamp
+        self.success = success
+   
 
